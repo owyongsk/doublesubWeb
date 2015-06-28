baf435062c568c6d9f5bbea87b17d8ce949b1ca8
SoonKhen OwYong <dude@owyong.sk>
2015-06-28 15:07:49 +0800

    remove the stream API as the requirement for res to send response

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var express   = require('express');
 var app       = express();
 var multer    = require('multer');
 var doublesub = require('doublesub');
-var fs        = require('fs');
-var Readable  = require('stream').Readable
 
 app.set('view engine', 'jade');
 
@@ -20,16 +18,14 @@ app.post('/',[ multer({ inMemory: true }), function(req, res){
     yandexKey: process.env.YANDEX_KEY
   }
   doublesub(optsObj, function(error, data){
-    if (error) { res.send("SHIT JUST HAPPENED!") }
+    if (error) {
+      res.send("SHIT JUST HAPPENED!")
+    } else {
+      res.setHeader('Content-disposition', 'attachment; filename=new.srt');
+      res.setHeader('Content-type',        'text/plain');
 
-    res.setHeader('Content-disposition', 'attachment; filename=new.srt');
-    res.setHeader('Content-type',        'text/plain');
-
-    var s = new Readable;
-    s.push(data);
-    s.push(null);
-
-    s.pipe(res);
+      res.send(data);
+    }
   });
 }]);
 
@@ -38,6 +34,6 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 
 });
