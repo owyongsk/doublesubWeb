9e4a97884467bd0673f0215eb8c43cf0975b0040
SoonKhen OwYong <dude@owyong.sk>
2015-07-02 09:19:22 +0800

    removed the useless xmlrpc to js objects and catch res from xmlrpc request

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ client = xmlrpc.createClient({ host: "api.opensubtitles.org", path: "/xml-rpc" }
 client.methodCall('LogIn', ["","","en","OSTestUserAgent"], function(err, res) {
   var token = res.token;
   client.methodCall('SearchSubtitles', [token, [{sublanguageid: "eng", moviehash: '51df0bf1a563628e', moviebytesize: '793302687'}]], function(err,res){
+    res.data // Arrays yeap
     debugger
   });
   client.methodCall('LogOut', [token], function(err,res){console.log("LOGGED OUT!");})
